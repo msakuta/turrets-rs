0777d0856c52dee393a2419b93fa97247c75bb2c
msakuta <masahiro.sakuta@gmail.com>
2022-07-11 23:58:11 +0900

    Add some rotating towers

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,26 @@ fn main() {
 }
 
 #[derive(Component)]
+struct Position(f64, f64);
+
+#[derive(Component)]
 struct Rotation(f64);
+
+#[derive(Component)]
+struct Bullet;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands
-        .spawn_bundle(SpriteBundle {
-            texture: asset_server.load("turret.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        })
-        .insert(Rotation(0.));
+
+    for i in 0..3 {
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: asset_server.load("turret.png"),
+                ..default()
+            })
+            .insert(Position(i as f64 * 100.0 - 100., 0.0))
+            .insert(Rotation(i as f64 * std::f64::consts::PI / 3.));
+    }
 }
 
 fn animate(time: Res<Time>, mut query: Query<&mut Rotation>) {
@@ -29,8 +38,10 @@ fn animate(time: Res<Time>, mut query: Query<&mut Rotation>) {
     }
 }
 
-fn sprite_transform(mut query: Query<(&Rotation, &mut Transform)>) {
-    for (rotation, mut transform) in query.iter_mut() {
-        transform.rotation = Quat::from_rotation_z(rotation.0 as f32);
+fn sprite_transform(mut query: Query<(&Position, &Rotation, &mut Transform)>) {
+    for (position, rotation, mut transform) in query.iter_mut() {
+        *transform = Transform::from_xyz(position.0 as f32, position.1 as f32, 0.)
+            .with_rotation(Quat::from_rotation_z(rotation.0 as f32))
+            .with_scale(Vec3::new(3., 3., 3.));
     }
 }
