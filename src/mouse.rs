a09ca13dc679eb349e47d142bffc84fcd07c6854
msakuta <masahiro.sakuta@gmail.com>
2022-08-03 00:15:23 +0900

    Allow dragging towers with mouse

--- src/mouse.rs
+++ src/mouse.rs
@@ -5,9 +5,12 @@ pub(crate) struct MousePlugin;
 
 impl Plugin for MousePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(SelectedTower { tower: None });
+        app.insert_resource(SelectedTower {
+            tower: None,
+            dragging: false,
+        });
         app.add_startup_system(setup);
-        app.add_system(mouse_position);
+        app.add_system(mouse_system);
     }
 }
 
@@ -29,12 +32,13 @@ pub(crate) struct MouseCursor;
 
 pub(crate) struct SelectedTower {
     pub tower: Option<Entity>,
+    pub dragging: bool,
 }
 
-fn mouse_position(
+fn mouse_system(
     windows: Res<Windows>,
     mut query: Query<(&mut Transform, &mut Visibility), With<MouseCursor>>,
-    query_towers: Query<(Entity, &Position), With<Tower>>,
+    mut query_towers: Query<(Entity, &mut Position), With<Tower>>,
     btn: Res<Input<MouseButton>>,
     mut selected_tower: ResMut<SelectedTower>,
 ) {
@@ -53,21 +57,38 @@ fn mouse_position(
             mouse_position.x - width / 2.,
             mouse_position.y - height / 2.,
         );
-        // println!("Mouse: {:?} -> {:?}", mouse_position, mouse_screen);
-        for (entity, tower_position) in query_towers.iter() {
-            if tower_position.0.distance(mouse_screen) < 30. {
-                visibility.is_visible = true;
-                *cursor_transform = Transform::from_xyz(tower_position.0.x, tower_position.0.y, 0.)
+
+        if selected_tower.dragging {
+            if let Some(mut tower) = selected_tower
+                .tower
+                .and_then(|tower| query_towers.get_component_mut::<Position>(tower).ok())
+            {
+                tower.0 = mouse_screen;
+                *cursor_transform = Transform::from_xyz(mouse_screen.x, mouse_screen.y, 0.)
                     .with_scale(Vec3::new(2., 2., 1.));
-
-                selected_tower.tower = Some(entity);
-
-                if btn.just_pressed(MouseButton::Left) {
-                    println!("Just_pressed! {:?} -> {:?}", mouse_position, mouse_screen);
-                }
-                return;
             }
+        } else {
+            // println!("Mouse: {:?} -> {:?}", mouse_position, mouse_screen);
+            for (entity, tower_position) in query_towers.iter() {
+                if tower_position.0.distance(mouse_screen) < 30. {
+                    visibility.is_visible = true;
+                    *cursor_transform =
+                        Transform::from_xyz(tower_position.0.x, tower_position.0.y, 0.)
+                            .with_scale(Vec3::new(2., 2., 1.));
+
+                    selected_tower.tower = Some(entity);
+
+                    if btn.just_pressed(MouseButton::Left) {
+                        println!("Just_pressed! {:?} -> {:?}", mouse_position, mouse_screen);
+                        selected_tower.dragging = true;
+                    }
+                    return;
+                }
+            }
+            visibility.is_visible = false;
         }
-        visibility.is_visible = false;
+    }
+    if btn.just_released(MouseButton::Left) {
+        selected_tower.dragging = false;
     }
 }
