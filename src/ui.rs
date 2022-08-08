2c72a5699b3a24b1c038954cfb5d38eea49684bc
msakuta <masahiro.sakuta@gmail.com>
2022-08-09 01:59:20 +0900

    Refactor: split ui/quit.rs

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,19 @@
+mod quit;
 mod tower_palette;
 
 use bevy::prelude::*;
 
-use self::tower_palette::{add_palette_buttons, palette_mouse_system};
+use self::{
+    quit::{
+        add_quit_button, quit_button_system, quit_event_system, show_quit_button_system,
+        HOVERED_BUTTON,
+    },
+    tower_palette::{add_palette_buttons, palette_mouse_system},
+};
 use crate::{
     mouse::SelectedTower,
     tower::{spawn_towers, TowerScore},
-    ClearEvent, Health, Level, Scoreboard, StageClear,
+    Health, Level, Scoreboard, StageClear,
 };
 
 pub(crate) struct UIPlugin;
@@ -24,12 +31,15 @@ impl Plugin for UIPlugin {
         app.add_system(palette_mouse_system);
         app.add_system(quit_event_system);
         app.add_system(quit_button_system);
-        app.add_system(difficulty_button_system);
         app.add_system(show_quit_button_system);
+        app.add_system(difficulty_button_system);
         app.add_system(show_difficulty_buttons_system);
         app.add_system(difficulty_event_system);
     }
 }
+
+struct StartEvent(usize);
+struct QuitEvent;
 
 #[derive(Component)]
 struct QuitButtonFilter;
@@ -67,8 +77,6 @@ const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
 const STATUS_FONT_SIZE: f32 = 20.0;
 
 const BUTTON_HEIGHT: f32 = 65.0;
-const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 
 const PALETTE_SIZE: f32 = 64.;
 
@@ -199,44 +207,6 @@ fn build_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     add_palette_buttons(&mut commands, &asset_server);
     add_difficulty_buttons(&mut commands, &asset_server);
     add_status_panel(&mut commands, &asset_server);
-}
-
-fn add_quit_button(commands: &mut Commands, asset_server: &Res<AssetServer>) {
-    commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(BUTTON_HEIGHT)),
-                margin: Rect::all(Val::Auto),
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    top: PADDING_PX,
-                    right: PADDING_PX,
-                    ..default()
-                },
-                ..default()
-            },
-            color: NORMAL_BUTTON.into(),
-            ..default()
-        })
-        .insert(QuitButtonFilter)
-        .with_children(|parent| {
-            parent
-                .spawn_bundle(TextBundle {
-                    text: Text::with_section(
-                        "Quit",
-                        TextStyle {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: SCOREBOARD_FONT_SIZE,
-                            color: TEXT_COLOR,
-                        },
-                        Default::default(),
-                    ),
-                    ..default()
-                })
-                .insert(QuitButtonFilter);
-        });
 }
 
 fn add_difficulty_buttons(commands: &mut Commands, asset_server: &Res<AssetServer>) {
@@ -425,52 +395,6 @@ fn update_tower_health(
     }
 }
 
-struct StartEvent(usize);
-struct QuitEvent;
-
-fn quit_button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut UiColor),
-        (Changed<Interaction>, With<Button>, With<QuitButtonFilter>),
-    >,
-    mut writer: EventWriter<QuitEvent>,
-    level: Res<Level>,
-) {
-    if let Level::Select = level.as_ref() {
-        return;
-    }
-    for (interaction, mut color) in interaction_query.iter_mut() {
-        match *interaction {
-            Interaction::Clicked => {
-                writer.send(QuitEvent);
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-            }
-            Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-            }
-        }
-    }
-}
-
-fn quit_event_system(
-    mut commands: Commands,
-    query: Query<Entity, With<StageClear>>,
-    mut level: ResMut<Level>,
-    mut reader: EventReader<QuitEvent>,
-    mut writer: EventWriter<ClearEvent>,
-) {
-    if reader.iter().last().is_some() {
-        println!("Received QuitEvent");
-        for entity in query.iter() {
-            commands.entity(entity).despawn();
-        }
-        *level = Level::Select;
-        writer.send(ClearEvent);
-    }
-}
-
 fn difficulty_event_system(
     mut reader: EventReader<StartEvent>,
     mut commands: Commands,
@@ -512,19 +436,6 @@ fn difficulty_button_system(
                 }
             }
         }
-    }
-}
-
-fn show_quit_button_system(
-    mut button_query: Query<&mut Visibility, With<QuitButtonFilter>>,
-    level: Res<Level>,
-) {
-    for mut button in button_query.iter_mut() {
-        button.is_visible = if let Level::Select = level.as_ref() {
-            false
-        } else {
-            true
-        };
     }
 }
 
---
+++ src/ui/quit.rs
@@ -0,0 +1,104 @@
+use bevy::prelude::*;
+
+use crate::{ClearEvent, Level, StageClear};
+
+use super::{
+    QuitButtonFilter, QuitEvent, BUTTON_HEIGHT, PADDING_PX, SCOREBOARD_FONT_SIZE, TEXT_COLOR,
+};
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+pub(super) const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+
+pub(super) fn add_quit_button(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(BUTTON_HEIGHT)),
+                margin: Rect::all(Val::Auto),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    top: PADDING_PX,
+                    right: PADDING_PX,
+                    ..default()
+                },
+                ..default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..default()
+        })
+        .insert(QuitButtonFilter)
+        .with_children(|parent| {
+            parent
+                .spawn_bundle(TextBundle {
+                    text: Text::with_section(
+                        "Quit",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: SCOREBOARD_FONT_SIZE,
+                            color: TEXT_COLOR,
+                        },
+                        Default::default(),
+                    ),
+                    ..default()
+                })
+                .insert(QuitButtonFilter);
+        });
+}
+
+pub(super) fn quit_button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut UiColor),
+        (Changed<Interaction>, With<Button>, With<QuitButtonFilter>),
+    >,
+    mut writer: EventWriter<QuitEvent>,
+    level: Res<Level>,
+) {
+    if let Level::Select = level.as_ref() {
+        return;
+    }
+    for (interaction, mut color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Clicked => {
+                writer.send(QuitEvent);
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+pub(super) fn quit_event_system(
+    mut commands: Commands,
+    query: Query<Entity, With<StageClear>>,
+    mut level: ResMut<Level>,
+    mut reader: EventReader<QuitEvent>,
+    mut writer: EventWriter<ClearEvent>,
+) {
+    if reader.iter().last().is_some() {
+        println!("Received QuitEvent");
+        for entity in query.iter() {
+            commands.entity(entity).despawn();
+        }
+        *level = Level::Select;
+        writer.send(ClearEvent);
+    }
+}
+
+pub(super) fn show_quit_button_system(
+    mut button_query: Query<&mut Visibility, With<QuitButtonFilter>>,
+    level: Res<Level>,
+) {
+    for mut button in button_query.iter_mut() {
+        button.is_visible = if let Level::Select = level.as_ref() {
+            false
+        } else {
+            true
+        };
+    }
+}
